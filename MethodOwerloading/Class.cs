e2319037f7aca8f804568fc4cc30c3cf1b7f77b4
Lutfu24 <lutfuhm@code.edu.az>
2023-07-20 17:44:54 +0400

    task

---
+++ MethodOwerloading/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOwerloading;
+
+internal class Student
+{
+    public void GetResult(double num)
+    {
+        Console.WriteLine(Math.Pow(num, num));
+    }
+
+    public void GetResult(double num, double num1)
+    {
+        Console.WriteLine(Math.Pow(num, num1));
+    }
+}
---
+++ MethodOwerloading/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using MethodOwerloading;
+
+double a = 2;
+double b = 3;
+
+Student student = new Student();
+
+student.GetResult(a,b);
+
+
+ 
